92e270d407fd15522675bb48458f8221f1e6669a
sukhamaya mohapatra <44130646+SukhamayaMohapatra@users.noreply.github.com>
2026-03-31 22:40:03 +0530

    2nd commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import HomePage from "./pages";
 
+export const metadata = {
+  title: "Austex Pharma",
+  description: "Healthcare Web Application",
+};
+
 export default function Home() {
   return (
     <div>
